ee5a44732013e4d00ee5fb8e95a3bbfabbb7b125
masterchrizz91-crypto <masterchrizz91@gmail.com>
2025-11-06 07:24:27 +0100

    final integration test pipeline.py

--- pipelines/abalone/pipeline.py
+++ pipelines/abalone/pipeline.py
@@ -200,7 +200,7 @@ def get_pipeline(
     )
     xgb_train.set_hyperparameters(
         objective="reg:linear",
-        num_round=40,
+        num_round=50,
         max_depth=5,
         eta=0.2,
         gamma=5,
